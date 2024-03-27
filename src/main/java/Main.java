08b9459d839bd8f272adedb5b2784e14f3a1bafc
radioactive1337 <vysotskiidanil@gmail.com>
2024-03-27 14:31:24 +0600

    test2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.selenium.dev/documentation/");
-        System.out.println(driver.getTitle());
+        WebElement h1 = driver.findElement(By.xpath("//div[@class='td-content']/h1"));
+        System.out.println(h1.getText());
+        System.out.println(driver.getCurrentUrl());
         driver.quit();
     }
 }
